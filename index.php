2b1ca5c7c0dba0cb3213f59a48001e10763854c3
syatirah <amirsyatirah@gmail.com>
2021-09-06 21:02:32 +0800

    try edit index

---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+    <head>
+        <title>Sherlock Holmes Online</title>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <style>
+			/* Column container */
+			.row {  
+				display: -ms-flexbox; /* IE10 */
+				display: flex;
+				-ms-flex-wrap: wrap; /* IE10 */
+				flex-wrap: wrap;
+			}
+		  
+            ul {
+                list-style-type: none;
+                font-family: Arial;
+                background-color: Black;
+                color: White;
+                overflow: hidden;
+            }
+
+            li {
+                float: Left;
+            }
+
+            li a {
+                display: block;
+                padding: 14px 16px;
+                color: White;
+                font-family: Arial;
+                background-color: Black;
+                text-decoration: none;
+            }
+
+            li a:hover {
+                background-color: Grey;
+                color: Skyblue;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Sherlock Holmes Online</h1>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="list.php">List of Books</a></li>
+            <li><a href="borrow.php">Borrow a Book</a></li>
+        </ul>
+
+        <div class="row">
+            <h2>List of Books</h2>
+            <br/>
+            <center>
+                <table border="1">
+                    <tr>
+                        <td width="30%">Picture</td>
+                        <td width="15%">Title</td>
+                        <td width="50%">Synopsis</td>
+                        <td width="5%"></td>
+                    </tr>
+                    <tr>
+                        <td align="center"><img src="https://almabooks.com/wp-content/uploads/2016/10/adventures-of-Sherlock-Holmes.jpg" style="width:300px"></td>
+                        <td align="center">The Adventures of Sherlock Holmes</td>
+                        <td>
+                            <p>From his Baker Street apartment, Sherlock Holmes wields his powers of deduction in pursuit of justice and truth, venturing out into
+                            foggy Victorian London accompanied by his faithful sidekick Dr Watson. This classic collection of Holmes tales includes many of the
+                            detective’s most-loved exploits: Holmes is confronted by a venomous snake in ‘The Adventure of the Speckled Band’, mystified by a missing
+                            thumb in ‘The Adventure of the Engineer’s Thumb’ and beguiled by a beautiful opera singer in ‘A Scandal in Bohemia’, never once losing his
+                            famous cool.</p>
+                            <p>First appearing separately in the Strand Magazine, these stories were published together in 1892 in a volume that rapidly became one of
+                            the most popular Sherlock Holmes collections. Showcasing Arthur Conan Doyle’s inimitable genius for mystery and storytelling, these tales
+                            are proof that the famous detective remains one of the greatest crime fighters ever created.</p>
+                        </td>
+                        <td align="center"><button class="button">Borrow</button></td>
+                    </tr>
+                </table>
+            </center>
+        </div>
+    <body>
+</html>
