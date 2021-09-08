3409d16e2188b3512b457cd64c6e0b53b57d05f8
syatirah <amirsyatirah@gmail.com>
2021-09-08 11:13:29 +0800

    try edit index

--- index.php
+++ index.php
@@ -4,41 +4,6 @@
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
         <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-        <style>
-			/* Column container */
-			.row {  
-				display: -ms-flexbox; /* IE10 */
-				display: flex;
-				-ms-flex-wrap: wrap; /* IE10 */
-				flex-wrap: wrap;
-			}
-		  
-            ul {
-                list-style-type: none;
-                font-family: Arial;
-                background-color: Black;
-                color: White;
-                overflow: hidden;
-            }
-
-            li {
-                float: Left;
-            }
-
-            li a {
-                display: block;
-                padding: 14px 16px;
-                color: White;
-                font-family: Arial;
-                background-color: Black;
-                text-decoration: none;
-            }
-
-            li a:hover {
-                background-color: Grey;
-                color: Skyblue;
-            }
-        </style>
     </head>
     <body>
         <h1>Sherlock Holmes</h1>
