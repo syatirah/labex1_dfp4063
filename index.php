77c5cbe9c710bd420ba7ddea0163a9b73aaa7269
syatirah <amirsyatirah@gmail.com>
2021-09-10 12:35:27 +0800

    labex1

--- index.php
+++ index.php
@@ -1,45 +1,34 @@
-<html>
-    <head>
-        <title>Sherlock Holmes</title>
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
-        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-    </head>
-    <body>
-        <h1>Sherlock Holmes</h1>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="menus.php">List of Books</a></li>
-        </ul>
+<!DOCTYPE html>
+<html lang="en">
 
-        <div class="row">
-            <h2>List of Books</h2>
-            <br/>
-            <center>
-                <table border="1">
-                    <tr>
-                        <td width="30%">Picture</td>
-                        <td width="15%">Title</td>
-                        <td width="50%">Synopsis</td>
-                        <td width="5%"></td>
-                    </tr>
-                    <tr>
-                        <td align="center"><img src="https://almabooks.com/wp-content/uploads/2016/10/adventures-of-Sherlock-Holmes.jpg" style="width:300px"></td>
-                        <td align="center">The Adventures of Sherlock Holmes</td>
-                        <td>
-                            <p>From his Baker Street apartment, Sherlock Holmes wields his powers of deduction in pursuit of justice and truth, venturing out into
-                            foggy Victorian London accompanied by his faithful sidekick Dr Watson. This classic collection of Holmes tales includes many of the
-                            detective’s most-loved exploits: Holmes is confronted by a venomous snake in ‘The Adventure of the Speckled Band’, mystified by a missing
-                            thumb in ‘The Adventure of the Engineer’s Thumb’ and beguiled by a beautiful opera singer in ‘A Scandal in Bohemia’, never once losing his
-                            famous cool.</p>
-                            <p>First appearing separately in the Strand Magazine, these stories were published together in 1892 in a volume that rapidly became one of
-                            the most popular Sherlock Holmes collections. Showcasing Arthur Conan Doyle’s inimitable genius for mystery and storytelling, these tales
-                            are proof that the famous detective remains one of the greatest crime fighters ever created.</p>
-                        </td>
-                        <td align="center"><button class="button">Borrow</button></td>
-                    </tr>
-                </table>
-            </center>
-        </div>
-    <body>
+<head>
+    <link href="style.css" rel="stylesheet">
+    <title>Sherlock Homes</title>
+</head>
+
+<body>
+
+    <div class="header">
+        <center>
+            <h1>Sherlock Homes</h1>
+        </center>
+
+    </div>
+    <div class="navbar">
+
+        <?php include "containingMenu.php"; ?>
+
+    </div>
+
+    <?php
+    echo "<p>HOME</p>";
+    ?>
+    <br>
+    <p>Sherlock Homes<p1>
+        <br><input type="image" src="book2.jpg"></br>
+
+
+        </var>
+</body>
+
 </html>
---
+++ sinopsis.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="style.css" rel="stylesheet">
+    <title>Sherlock Homes</title>
+</head>
+
+<body>
+
+    <div class="header">
+        <center>
+            <h1>Sherlock Homes</h1>
+        </center>
+
+    </div>
+
+    <div class="navbar">
+
+        <?php include "containingMenu.php"; ?>
+
+    </div>
+    <?php
+    echo "<p> SINOPSIS </p>";
+    ?>
+
+    <center>
+        <div>PENULIS: CONAN DOYLE
+            TAJUK: SHERLOCK HOMES
+            HARGA : RM40 (SM)/RM60(SS)
+
+            <p1>When a string of brutal murders terrorizes London, it doesn't take long for legendary detective Sherlock Holmes (Robert Downey Jr.) and his crime-solving partner, Dr. Watson (Jude Law), to find the killer, Lord Blackwood (Mark Strong). A devotee of the dark arts, Blackwood has a bigger scheme in mind, and his execution plays right into his plans. The game is afoot when Blackwood seems to rise from the grave, plunging Holmes and Watson into the world of the occult and strange technologies.
+        </div>
+        </p1>
+    </center>
+</body>
+
+</html>
